58f39549303092c916eed7a1bfb44320d526acad
kuetabby <vansmuhammad@gmail.com>
2025-01-15 00:24:52 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,6 @@ import { Toaster } from "react-hot-toast";
 import AppBar from "./components/AppBar";
 import Footer from "./components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const futura = localFont({
   src: "./fonts/Futura.ttf",
   variable: "--font-geist-mono",
